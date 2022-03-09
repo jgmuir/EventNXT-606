f1c3362163ed080230f03d9bffc61bb3fc0e6386
Edmund Do <esd@tamu.edu>
2022-03-09 15:42:24 -0500

    fix: conflicts with migration

--- db/migrate/20220308001540_update_database.rb
+++ db/migrate/20220308001540_update_database.rb
@@ -1,43 +1,47 @@
 class UpdateDatabase < ActiveRecord::Migration[6.1]
   def change
-    add_column :seating_types, :Name, :string
-    add_column :seating_types, :MaxSeats, :integer
-    add_column :seating_types, :Price, :integer
-    remove_column :seating_types, :box_office_seat_count
-    remove_column :seating_types, :balance_seats
-    remove_column :seating_types, :box_office_seat_count
-    remove_column :seating_types, :vip_seats_count
+    create_table :seats, force: :cascade do |t|
+      t.references :events, null: false, foreign_key: true
+      t.string :category, null: false
+      t.integer :total_count
+      t.float :price
+    end
+
+    create_table :guests, force: :cascade do |t|
+      t.references :events, null: false, foreign_key: true
+      t.references :users, null: false, foreign_key: true
+      t.string :email, null: false
+      t.string :first_name
+      t.string :last_name
+      t.string :affiliation
+      t.string :type
+      t.boolean :booked, null: false, default: false
+      t.datetime :invite_expiration
+      t.datetime :referral_expiration
+      t.datetime :invited_at
+    end
+    rename_column :guests, :users_id, :added_by
+    add_index :guests, :email, unique: true
     
-    add_column :guests, :InvitationExpiration, :Date
-    add_column :guests, :ReferralExpiration, :Date
-    add_column :guests, :LastEmailed, :Date
-    remove_column :guests, :seat_category
-    remove_column :guests, :max_seats_num
-    
-    create_table :ReferralReward do |t|
-      t.string :Rewards
-      t.integer :MinReferrals
+    create_table :referral_rewards, force: :cascade do |t|
+      t.references :events, null: false, foreign_key: true
+      t.string :reward
+      t.integer :min_count, default: 0
     end
     
-    create_table :Guest_SeatCategory do |t|
-    t.intger :CommittedSeats
-    t.integer :MaxPossibleSeats
-    t.bigint "guest_id"
-    t.bigint "seat_category_id"
-    
+    create_table :guest_seat_tickets do |t|
+      t.references :guests, null: false, foreign_key: true
+      t.references :seats, null: false, foreign_key: true
+      t.integer :committed
+      t.integer :allotted
     end
     
-    create_table :Guest_ReferralReward do |t|
-      t.string :guesttoken
-      t.integer :Referrals
-      t.bigint "guest_id"
-      t.bigint "referral_id"
-   end
-    add_foreign_key :Guest_ReferralReward, :guests, column: :guest_id, primary_key: :id
-    add_foreign_key :Guest_ReferralReward, :ReferralReward, column: :referral_id, primary_key: :id
-    add_foreign_key :Guest_SeatCategory, :guests, column: :guest_id, primary_key: :id
-    add_foreign_key :Guest_SeatCategory, :SeatCategory, column: :guest_id, primary_key: :id
-    add_foreign_key :ReferralReward, :events, column: :event_id, primary_key: :id
-    add_foreign_key :SeatCategory, :events, column: :event_id, primary_key: :seat_category_id
+    create_table :guest_referral_rewards do |t|
+      t.references :guests, null: false, foreign_key: true
+      t.references :referral_rewards, null: false, foreign_key: true
+      t.integer :count, default: 0
+    end
+
+    add_reference :users, :events, foreign_key: true
   end
 end
