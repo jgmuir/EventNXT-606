9e417ca45ce8586d054fcf256ceeb968f39f518a
Edmund Do <esd@tamu.edu>
2022-04-29 02:14:46 -0400

    csrf skip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #before_action :authenticate_user!, :except => []
-  protect_from_forgery prepend: true, with: :exception
+  # protect_from_forgery prepend: true, with: :exception
 
   protected
 
